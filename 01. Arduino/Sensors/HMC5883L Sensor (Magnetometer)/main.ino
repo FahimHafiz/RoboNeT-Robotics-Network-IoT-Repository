4807d0dd2a90a70814f08653159410337ce90a4b
Shorower Hossain <42220653+shorower@users.noreply.github.com>
2025-02-21 03:21:57 +0600

    Create main.ino

---
+++ 01. Arduino/Sensors/HMC5883L Sensor (Magnetometer)/main.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <HMC5883L_Simple.h>
+
+HMC5883L_Simple Compass;
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+    
+  Compass.SetDeclination(-0, 23, 'W');  
+  Compass.SetSamplingMode(COMPASS_SINGLE);
+  Compass.SetScale(COMPASS_SCALE_130);
+  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+}
+
+// Our main program loop.
+void loop()
+{
+   float heading = Compass.GetHeadingDegrees();
+   
+   Serial.print("Heading: \t");
+   Serial.println( heading );   
+   delay(1000);
+}
+
