20955ffd8656f3211a815c91de04bfd72bf3e49b
Shorower Hossain <42220653+shorower@users.noreply.github.com>
2025-03-03 15:31:41 +0600

    Update main.ino

--- 01. Arduino/Sensors/HMC5883L Sensor (Magnetometer)/main.ino
+++ 01. Arduino/Sensors/HMC5883L Sensor (Magnetometer)/main.ino
@@ -1,26 +1,47 @@
-#include <Arduino.h>
 #include <Wire.h>
-#include <HMC5883L_Simple.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 
-HMC5883L_Simple Compass;
-void setup()
-{
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+void setup(void) {
   Serial.begin(9600);
-  Wire.begin();
-    
-  Compass.SetDeclination(-0, 23, 'W');  
-  Compass.SetSamplingMode(COMPASS_SINGLE);
-  Compass.SetScale(COMPASS_SCALE_130);
-  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+
+
+  if (!mag.begin())  {
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while (1);
+  }
 }
 
-// Our main program loop.
-void loop()
-{
-   float heading = Compass.GetHeadingDegrees();
-   
-   Serial.print("Heading: \t");
-   Serial.println( heading );   
-   delay(1000);
+void loop(void) {
+  readCompass();
 }
 
+
+
+
+void readCompass() {
+  /* Get a new sensor event */
+  sensors_event_t event;
+  mag.getEvent(&event);
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  float declinationAngle = ((-0) + (29 / 60.0)) / (180 / M_PI);
+  heading += declinationAngle;
+
+  // Correct for when signs are reversed.
+  if (heading < 0)
+    heading += 2 * PI;
+
+  // Check for wrap due to addition of declination.
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180 / M_PI;
+
+  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+
+  delay(500);
+}
